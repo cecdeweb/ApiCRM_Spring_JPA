10c567a1422ac3236bdc7e064a6c2d9b88d599b6
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-06-29 22:04:58 +0200

    init project CRM

---
+++ src/test/java/com/m2i/ApiFilRougeCrm/ApiFilRougeCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.ApiFilRougeCrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFilRougeCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3f61bd2fdbb33a27bb6e64f50d7ce2255fe934d2
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-06 21:59:01 +0200

    add get client by Id

--- src/main/java/com/m2i/ApiFilRougeCrm/controller/ClientController.java
+++ src/main/java/com/m2i/ApiFilRougeCrm/controller/ClientController.java
@@ -3,10 +3,7 @@ package com.m2i.ApiFilRougeCrm.controller;
 import com.m2i.ApiFilRougeCrm.entity.Client;
 import com.m2i.ApiFilRougeCrm.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +23,8 @@ public class ClientController {
         clientService.createClient(client);
     }
 
+    @GetMapping("clients/{id}")
+    public Client getClient(@PathVariable Long id){
+        return clientService.getClient(id);
+    }
 }

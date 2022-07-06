bf9d70097340c0afa722fee33fd1e460e69fae2d
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-06 21:44:29 +0200

    method create client

--- src/main/java/com/m2i/ApiFilRougeCrm/controller/ClientController.java
+++ src/main/java/com/m2i/ApiFilRougeCrm/controller/ClientController.java
@@ -4,6 +4,8 @@ import com.m2i.ApiFilRougeCrm.entity.Client;
 import com.m2i.ApiFilRougeCrm.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,11 @@ public class ClientController {
     @GetMapping("clients")
     public List<Client> getClients() {
         return clientService.getAllClients();
+    }
+
+    @PostMapping("clients")
+    public void createClient(@RequestBody Client client){
+        clientService.createClient(client);
     }
 
 }
--- src/main/java/com/m2i/ApiFilRougeCrm/controller/OrderController.java
+++ src/main/java/com/m2i/ApiFilRougeCrm/controller/OrderController.java
@@ -27,9 +27,5 @@ public class OrderController {
         orderService.createOrder(order);
     }
 
-//    @PostMapping("order")
-//    public List<Order> saveOrder(Order order){
-//       return orderService.saveOrder();
-//    }
 
 }
